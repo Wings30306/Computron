78d4b040aa208a1d7897817ef8bc8319eca3ca9e
Joke Heyndels <jo_hannah@outlook.com>
2020-09-20 11:40:01 +0200

    Initial commit: starting code from Codecademy project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Computron
+{
+    using System;
+
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            TodoList tdl = new TodoList();
+            tdl.Add("Invite friends");
+            tdl.Add("Buy decorations");
+            tdl.Add("Party");
+
+            PasswordManager pm = new PasswordManager("iluvpie", true);
+        }
+    }
+}
